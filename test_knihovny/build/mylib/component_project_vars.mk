a10b32f1577b1a14d1f284f8522d3e2bd58a00d1
Marcel Juchelka <xer@seznam.cz>
2021-12-27 07:46:23 +0100

    Delete test_knihovny/build directory
    smayano

--- test_knihovny/build/mylib/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/mylib/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/mylib -lmylib
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += mylib
-COMPONENT_LDFRAGMENTS += 
-component-mylib-build: 
